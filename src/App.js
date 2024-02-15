21325cb98d5e9bfa5ead1b7c34e966d4ceb9924d
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-02-15 10:03:02 +0000

    Added link to Github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <p>
+        This project was coded by Lucie Barbier-Dearnley. Open-sourced on{" "}
+        <a
+          href="https://github.com/luciebarbierdearnley/react-weather-app"
+          target="_blank"
+          rel="noreferrer">
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
