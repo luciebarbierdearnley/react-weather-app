ea4f2f4c1057469007a47735b4d06657bce1d0ce
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-02-16 11:08:37 +0000

    Added gitHub profile and netlify links

--- src/App.js
+++ src/App.js
@@ -7,13 +7,28 @@ export default function App() {
       <Weather defaultCity="London" />
 
       <footer>
-        This project was coded by Lucie Barbier-Dearnley. Open-sourced on{" "}
+        This project was coded by{" "}
+        <a
+          href="https://github.com/luciebarbierdearnley"
+          target="_blank"
+          rel="noreferrer">
+          Lucie Barbier-Dearnley
+        </a>
+        . Open-sourced on{" "}
         <a
           href="https://github.com/luciebarbierdearnley/react-weather-app"
           target="_blank"
           rel="noreferrer">
           GitHub
         </a>
+        . Hosted on{" "}
+        <a
+          href="https://react-weather-application-lbd.netlify.app/"
+          target="_blank"
+          rel="noreferrer">
+          Netlify
+        </a>
+        .
       </footer>
     </div>
   );
